26f02d9be849b3ae71d34bd23e63480e7b59de96
Soroush <soroush.raw@gmail.com>
2022-11-30 11:54:30 +0100

    Header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Layout/Header";
+import {Fragment} from "react"
 function App() {
-    return (
-        <div>
-            <h2>Let's get started!</h2>
-        </div>
-    );
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import jordan from "../../assets/jordan.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1> Jordan. </h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes.mainImage}>
+        <img src={jordan} alt="jordan brand" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
